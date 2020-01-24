1f9d50d9d5b716e1a373d18d22df7329b54937b5
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-01-24 12:41:10 -0600

    added yargs for commandline entry

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
 const validator = require('validator')
 const notes = require('./notes.js')
 const chalk = require('chalk')
+const yargs = require('yargs')
+
+const command = process.argv
+const yargsCommand = yargs.argv
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove the note',
+    handler: function () {
+        console.log('Removing note!')
+    }
+})
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption:true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'This will list all the notes',
+    handler: function () {
+        console.log('Listing notes!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+yargs.parse()
 
 
-console.log(notes());
-
-console.log(validator.isURL('https:/mead.io'))
-
-console.log(chalk.bgGreen.inverse.bold("Successful!"))
